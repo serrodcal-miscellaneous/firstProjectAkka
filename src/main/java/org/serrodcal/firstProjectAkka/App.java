3a8c2052f6e74e98b6bbdddc0bb6c68c162e2567
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-18 08:55:27 +0200

    Initial commit

---
+++ src/main/java/org/serrodcal/firstProjectAkka/App.java
@@ -0,0 +1,13 @@
+package org.serrodcal.firstProjectAkka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
