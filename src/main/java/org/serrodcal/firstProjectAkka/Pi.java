daa6291cf778b13a320c0b6c8974779977e02fee
sergio.rodriguez <sergio.rodriguez@aptus.es>
2015-09-18 09:05:56 +0200

    Create Pi clase according to Akka web tutorial

---
+++ src/main/java/org/serrodcal/firstProjectAkka/Pi.java
@@ -0,0 +1,14 @@
+package org.serrodcal.firstProjectAkka;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.actor.UntypedActorFactory;
+import akka.routing.RoundRobinRouter;
+import akka.util.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Pi {
+
+}
